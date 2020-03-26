de93429dabf6727aaba246fb5ad121bfda9ab493
AkshatMishra01 <mishraakshat924@gmail.com>
2020-03-26 21:11:15 +0530

    R171219004: added division function

--- calculator.cpp
+++ calculator.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
-float substract(float x,float y);
+float substract(float x, float y);
 float add(float x, float y);
+float division(float x, float y);
 int main()
 {
     float number_1; 
     float number_2; 
     float sum_of_numbers;
     float diff_of_numbers;
+    float division_of_numbers;
     cout << "Enter the first number" << endl;
     cin>>number_1;
     cout << "Enter the second number" << endl;
     cin>>number_2;
     sum_of_numbers=add(number_1, number_2);
     diff_of_numbers=substract(number_1, number_2);
+    division_of_numbers=division(number_1,number_2);
     cout<<"sum of two numbers "<<sum_of_numbers<<endl;
     cout<<"positive difference of 2 numbers is"<<diff_of_numbers<<endl;
+    cout<<"division of 2 numbers is"<<division_of_numbers;
 }
 float add(float x, float y)
 {
@@ -27,4 +31,11 @@ if(x>y)
 	return (x-y);
 if(y>x)
 	return (y-x);
+}
+float division(float x, float y)
+{
+if(y=0)
+	cout<<"invalid";
+else
+	return (x/y);
 }
