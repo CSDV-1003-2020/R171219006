9f92492b0d8171518148d8c90d3339f911915984
animesh-deka <animesh7637@gmail.com>
2020-03-26 20:43:47 +0530

    R171219005:Added multiply function

--- calculator.cpp
+++ calculator.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 float add(float x, float y);
+float multiply(float x, float y);
 int main()
 {
     float number_1; 
     float number_2; 
     float sum_of_numbers;
+    float product_of_numbers;
     cout << "Enter the first number" << endl;
     cin>>number_1;
     cout << "Enter the second number" << endl;
     cin>>number_2;
     sum_of_numbers=add(number_1, number_2);
+    product_of_numbers=multiply(number_1, number_2);
     cout<<"sum of two numbers "<<sum_of_numbers<<endl;
+    cout<<"product of two numbers"<<product_of_numbers<<endl;
 }
 
 
@@ -20,3 +24,9 @@ float add(float x, float y)
 {
     return (x+y);
 }
+
+float multiply(float x, float y)
+{
+	return (x*y);
+}
+
