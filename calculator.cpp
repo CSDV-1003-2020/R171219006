d38b0b283d67a0a96b7b26e8c4e2753349225d90
AkshatMishra01 <mishraakshat924@gmail.com>
2020-03-27 19:24:03 +0530

    R171219004: added square root function

--- calculator.cpp
+++ calculator.cpp
@@ -3,6 +3,7 @@ using namespace std;
 float substract(float x, float y);
 float add(float x, float y);
 float division(float x, float y);
+float greater(float x, float y);
 int main()
 {
     float number_1; 
@@ -10,6 +11,7 @@ int main()
     float sum_of_numbers;
     float diff_of_numbers;
     float division_of_numbers;
+    float greater_number;
     cout << "Enter the first number" << endl;
     cin>>number_1;
     cout << "Enter the second number" << endl;
@@ -17,9 +19,18 @@ int main()
     sum_of_numbers=add(number_1, number_2);
     diff_of_numbers=substract(number_1, number_2);
     division_of_numbers=division(number_1,number_2);
+    greater_number=greater(number_1,number_2);
     cout<<"sum of two numbers "<<sum_of_numbers<<endl;
     cout<<"positive difference of 2 numbers is"<<diff_of_numbers<<endl;
     cout<<"division of 2 numbers is"<<division_of_numbers;
+    return 0;
+}
+float greater(float x, float y)
+{
+if(x>y)
+	cout<<number_1<<" is greater than "<<number_2;
+else
+	cout<<number_2<<" is greater than "<<number_1;
 }
 float add(float x, float y)
 {
