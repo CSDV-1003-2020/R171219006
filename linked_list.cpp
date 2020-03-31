41fe77e0752b9fa4e60b914adf5471958a665f92
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-03-31 18:06:17 +0530

    R171219006: Removed function for reversing a linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -361,35 +361,25 @@ void single_llist::display()
     cout<<"NULL"<<endl;
 }
 
-/*
- * Reverse Link List
+ /*
+ * Searching an element
  */
-void single_llist::reverse()
+void single_llist::search()
 {
-    struct node *ptr1, *ptr2, *ptr3;
-    int value, pos = 0;
-    bool flag = false;
-    if (start == NULL)
+    cout<<"Enter the value to be searched: ";
+    cin>>value;
+    struct node *s;
+    s = start;
+    while (s != NULL)
     {
-        cout<<"List is empty"<<endl;
-        return;
+        pos++;
+        if (s->info == value)
+        {
+            flag = true;
+            cout<<"Element "<<value<<" is found at position "<<pos<<endl;
+        }
+        s = s->next;
     }
-    if (start->next == NULL)
-    {
-        return;
-    }  
-    ptr1 = start;
-    ptr2 = ptr1->next;
-    ptr3 = ptr2->next;
-    ptr1->next = NULL;
-    ptr2->next = ptr1;
-    while (ptr3 != NULL)
-    {
-        ptr1 = ptr2;
-        ptr2 = ptr3;
-        ptr3 = ptr3->next;
-        ptr2->next = ptr1;         
-    }
-    start = ptr2;
+    if (!flag)
+        cout<<"Element "<<value<<" not found in the list"<<endl;  
 }
-
