c38a542f667e2444b9e695c1e4005e12242f2a23
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-03-31 16:58:59 +0530

    R171219006: Added a function for searching an element

--- linked_list.cpp
+++ linked_list.cpp
@@ -341,3 +341,32 @@ void single_llist::sort()
     }
 }
 
+/*
+ * Searching an element
+ */
+void single_llist::search()
+{
+    int value, pos = 0;
+    bool flag = false;
+    if (start == NULL)
+    {
+        cout<<"List is empty"<<endl;
+        return;
+    }
+    cout<<"Enter the value to be searched: ";
+    cin>>value;
+    struct node *s;
+    s = start;
+    while (s != NULL)
+    {
+        pos++;
+        if (s->info == value)
+        {
+            flag = true;
+            cout<<"Element "<<value<<" is found at position "<<pos<<endl;
+        }
+        s = s->next;
+    }
+    if (!flag)
+        cout<<"Element "<<value<<" not found in the list"<<endl;  
+}
