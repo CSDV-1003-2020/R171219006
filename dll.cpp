05384109e0df6f0d97a6e2ccc845095552c5620e
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-04-20 17:23:50 +0530

    R171219004: added function for reverse a doubly linked list

--- dll.cpp
+++ dll.cpp
@@ -114,3 +114,23 @@ int main()
     }
     return 0;
 }
+/*
+ * Reverse Doubly Link List
+ */
+void double_llist::reverse()
+{
+    struct node *p1, *p2;
+    p1 = start;
+    p2 = p1->next;
+    p1->next = NULL;
+    p1->prev = p2;
+    while (p2 != NULL)
+    {
+        p2->prev = p2->next;
+        p2->next = p1;
+        p1 = p2;
+        p2 = p2->prev; 
+    }
+    start = p1;
+    cout<<"List Reversed"<<endl; 
+}
